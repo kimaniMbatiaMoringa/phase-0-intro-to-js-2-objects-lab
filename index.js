b0e91eb9c107fcf4a152b5a295add7a5fa764f4a
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-06-16 20:45:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution in this file!
+
+let employee={ 
+    name:'Sam',
+    streetAddress:'Dennis Pritt Road'
+ }
+
+
+function updateEmployeeWithKeyAndValue(employee,key, value){
+
+    employee ={...employee}
+    employee[key]=value
+    //employees[`${key}`]=value,
+    return employee
+
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key, value){
+    employee[key]=value         //Tells object to change the employee "key" to the value of "value"
+    return employee
+
+}
+
+function deleteFromEmployeeByKey(object, key){
+    var employees={...object}
+    delete employees[key]
+    return employees
+}
+
+function destructivelyDeleteFromEmployeeByKey(object,key){
+    let employee1 ={...object}
+    delete employee1[key]
+    delete employee[key]
+    return employee1, employee
+}
+
+
+
+//updateEmployeeWithKeyAndValue(employee,"streetAddress","11 Broadway")
+//destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress" , "12 Broadway")
+
+destructivelyDeleteFromEmployeeByKey(employee,'name')
+console.log(employee.streetAddress)
+
+//deleteFromEmployeeByKey(employee,`name`)
+//console.log(employees)
+
+
+
+//let objectName="name"
+//console.log(employee[objectName])
+//console.log(employee.district.constit)
